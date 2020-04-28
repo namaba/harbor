67a96ea5b7a8c559e444fc4a515e2c58baae8c3f
namaba <one.2426@gmail.com>
2020-04-29 00:42:00 +0900

    feat: 管理画面にタレント新規・詳細・一覧追加

--- app/controllers/admin/talents_controller.rb
+++ app/controllers/admin/talents_controller.rb
@@ -1,16 +1,39 @@
 class Admin::TalentsController < Admin::ApplicationController
+	before_action :set_talent, only: %i[show]
+
 	def index
+		@talents = Talent.all
 	end
 
 	def new
-		
+		@talent = Talent.new
 	end
 
 	def create
-		
+		@talent = Talent.new(talent_params)
+		if @talent.save
+			redirect_to @talent, notice: '登録しました'
+		else
+			flash[:alert] = @talent.errors.full_messages.join("\n")
+			render :new
+		end
 	end
 
-	def show
+	def show; end
+
+	private
+	def set_talent
+		@talent = Talent.find params[:id]
+	end
+	
+	def talent_params
+		params.require(:talent).permit(
+			:name,
+			:instagram_url,
+			:youtube_url,
+			:memo,
+			:image,
+		)
 		
 	end
 end

cdd18076a41acefc735858d56497b98498cf7c27
namaba <one.2426@gmail.com>
2020-05-15 23:38:26 +0900

    feat: select2追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,12 @@
 import "bootstrap-material-design";
 import "../stylesheets/application";
 
+import "../states.js"
+
 require("@rails/ujs").start();
 require("@rails/activestorage").start();
 require("channels");
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

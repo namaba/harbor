7bb21f97ead44acebe2e4aba24a0d2c526bc6978
namaba <one.2426@gmail.com>
2020-05-16 00:15:09 +0900

    feat: select2追加

--- app/javascript/states.js
+++ app/javascript/states.js
@@ -3,5 +3,12 @@ import 'select2/dist/css/select2.css'
 import 'select2'
 
 window.addEventListener('DOMContentLoaded', () => {
-  $('.js-source-states').select2()
+  $('.select2').select2(
+    {
+      dropdownAutoWidth: true,
+      width: '100%',
+      placeholder: "Classic Theme",
+      theme: "classic"
+    }
+  )
 })

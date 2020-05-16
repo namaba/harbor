2dfb37772b5e757599c21acec0fe9a6c27a80c77
namaba <one.2426@gmail.com>
2020-05-16 16:29:51 +0900

    feat: タレント一覧追加

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -1,6 +1,10 @@
 class TalentsController < ApplicationController
   before_action :set_talent, only: %i[show archives]
 
+  def index
+    @talents = Talent.includes(:lives).all.limit(10)
+  end
+
   def show
     talent_ids = Live.future.pluck(:talent_id)
     @recomend_talents = Talent.find talent_ids.sample(4)
--- app/models/talent.rb
+++ app/models/talent.rb
@@ -13,35 +13,35 @@
 #  updated_at    :datetime         not null
 #
 class Talent < ApplicationRecord
-	mount_uploader :image, ImageUploader
-	include Redis::Objects
+  mount_uploader :image, ImageUploader
+  include Redis::Objects
 
-	has_many :lives, class_name: 'Live'
-	has_many :talent_categories
-	has_many :categories, through: :talent_categories
+  has_many :lives, class_name: 'Live'
+  has_many :talent_categories
+  has_many :categories, through: :talent_categories
 
-	sorted_set :ranking, global: true
+  sorted_set :ranking, global: true
 
-	def ranking_increment
-		ranking.increment(self.id)
-	end
+  def ranking_increment
+    ranking.increment(self.id)
+  end
 
-	def total_pv
-		ranking[self.id].to_i + pv.to_i
-	end
+  def total_pv
+    ranking[self.id].to_i + pv.to_i
+  end
 
-	private
+  private
 
-	# 　csvファイルからタレントデータをインポート
-	def import_csv
-		data_list = CSV.read("live_matome.csv")
-		data_list.each_with_index do |d, i|
-			if i > 188
-				if d[0]
-					t = Talent.new({name: d[0], memo: d[1], instagram_url: d[3], youtube_url: d[4]})
-					t.save
-				end
-			end
-		end
-	end
+  # 　csvファイルからタレントデータをインポート
+  def import_csv
+    data_list = CSV.read("live_matome.csv")
+    data_list.each_with_index do |d, i|
+      if i > 188
+        if d[0]
+          t = Talent.new({ name: d[0], memo: d[1], instagram_url: d[3], youtube_url: d[4] })
+          t.save
+        end
+      end
+    end
+  end
 end
